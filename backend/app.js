2163f78dbc1a558fb1a796f3aeb58c9fb624449e
Andy Nguyen <andy.nguyen@microchip.com>
2021-10-29 22:36:30 -0700

    pls

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,7 @@ app.use(fileupload())
 app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/frontend/build/index.html'));
+  res.sendFile(path.join(__dirname + '../frontend/build/index.html'));
 });
 
 
