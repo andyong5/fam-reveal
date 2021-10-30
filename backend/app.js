a8b4b902eb6dd5b76ac55b1758dcf1b9c78bd996
Andy Nguyen <andy.nguyen@microchip.com>
2021-10-29 20:44:16 -0700

    pls

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,6 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const revealRouter = require('./routes/reveal');
 const addRouter = require('./routes/add');
-const cors = require('cors')
 const app = express();
 
 // view engine setup
@@ -20,8 +19,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'build')));
 app.use(fileupload())
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -32,30 +36,6 @@ app.use('/reveal', revealRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
-const whitelist = ['http://localhost:3000', 'http://localhost:9000', 'https://fam-reveal-iota-phi.herokuapp.com/']
-const corsOptions = {
-  origin: function (origin, callback) {
-    console.log("** Origin of request " + origin)
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      console.log("Origin acceptable")
-      callback(null, true)
-    } else {
-      console.log("Origin rejected")
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-app.use(cors(corsOptions))
-
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'frontend/build')));
-// Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
-  });
-}
 
 
 // error handler
