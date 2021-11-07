571b8306004222bd46ce378a7ba9948da725e135
Andy Nguyen <acnguye@ucdavis.edu>
2021-11-06 18:28:50 -0700

    using database url

--- backend/routes/db.js
+++ backend/routes/db.js
@@ -8,8 +8,9 @@ const pg = require("pg");
 //   password: "Microsemi**2",
 //   database: "pledges_list",
 // });
-const url =
-  "postgres://gacsncbmlletln:e6cc527d0f42a64ff046a490134abefb97b4f05b0219c73ad5e48806d75b9bed@ec2-18-210-95-55.compute-1.amazonaws.com:5432/dbfd4d1q5226fk";
+// const url =
+//   "postgres://gacsncbmlletln:e6cc527d0f42a64ff046a490134abefb97b4f05b0219c73ad5e48806d75b9bed@ec2-18-210-95-55.compute-1.amazonaws.com:5432/dbfd4d1q5226fk";
+const url = process.env.DATABASE_URL;
 const pool = new pg.Pool({
   connectionString: url,
   ssl: {
